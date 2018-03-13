bb76489783a2b5cfb1d7a82c902fa69306ef9e97
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-03-13 23:06:08 +0100

    5

--- components/Search.js
+++ components/Search.js
@@ -1,44 +1,41 @@
 Search = React.createClass({
-    render: function() {
-        var styles = {
-            fontSize: '1.5em',
-            width: '90%',
-            maxWidth: '350px'
+    getInitialState(){
+        return {
+          searchingText: ''  
         };
-
-    return <input
-             type="text"
-             onChange={this.handleChange}
-             placeholder="Tutaj wpisz wyszukiwaną frazę"
-             style={styles}
-             value={this.state.searchTerm}/>
- 
-  handleChange: function(event) {
-    var searchingText = event.target.value;
-    this.setState({searchingText: searchingText});
-
-    if (searchingText.length > 2) {
-      this.props.onSearch(searchingText);
-    }
-  },
-
-  handleKeyUp: function(event) {
-    if (event.keyCode === 13) {
-      this.props.onSearch(this.state.searchingText);
-    }
-  },
-
-render: function() {
-    var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
-
-   
-  }
+    },
+    handleChange: function(event) {
+        var searchingText = event.target.value; // przypisujemy do zmiennej znaki wpisane na klawiaturze
+        console.log(searchingText);
+        this.setState({							//ustawiamy stan searchingText - nadajemy mu wartość wpisanego tekstu
+            searchingText: searchingText
+        });
+        if (searchingText.length>2){
+            this.props.onSearch(searchingText); // ??????????? - to wywołuje funkcję handleSwearch w App???
+        }
+    },
+    handleKeyUp: function(event){
+        if(event.keyKode === 13){
+            this.props.onSearch(this.state.searchingText); // ??????????????? to j.w.?????
+        }
+    },
     
-  }
+    render: function (){
+       var styles: {
+           fontSize: '1,5em ',
+           width: '90%',
+           maxWidth: '350px'
+       };
+       
+       return (
+          <input 
+          type='text'
+          onChange={this.handleChange} //wywołuje funkcję handleChange
+          onKeyUp={this.handleKeyUp} //wywołuje funkcję handleKeyUp
+          placeholder='Tutaj wpisz wyszukiwana frazę'
+          style={styles}
+          value={this.state.searchTerm} //???????????????
+          />
+       ); 
+   } 
 });
-    
-        getInitialState() {
-  return {
-    searchingText: ''
-  };
-}
